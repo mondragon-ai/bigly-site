3968bbeb62aa362e74c43d798a23547ff9846205
mondragon-ai <angel.mondragon@yahoo.com>
2024-12-09 19:52:20 -0300

    🎬 init site - bigly

---
+++ custom.js
@@ -0,0 +1,112 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const mainElement = document.querySelector("main");
+  const headerElement = document.querySelector(".navbar");
+  const headerHeight = headerElement.offsetHeight;
+
+  // Section One Elements
+  const sectionOneElement = document.querySelector(".sectionOne");
+  const sectionOneHeaders = sectionOneElement.querySelectorAll(".txt h1");
+  const thatHdr = sectionOneElement.querySelector(".txt .abs");
+  const sectionOneLogo = sectionOneElement.querySelector(".sectionOne img");
+  const sectionOneP = sectionOneElement.querySelector(".txt p");
+
+  // Section Two Elements
+  const sectionTwoEl = document.querySelector(".sectionTwo");
+  const sectionTwoHdr = sectionTwoEl.querySelector(".txt h1");
+  const sectionTwoP = sectionTwoEl.querySelector(".txt p");
+
+  mainElement.addEventListener("scroll", () => {
+    const scrollY = mainElement.scrollTop;
+    const sectionOneHeight = sectionOneElement.getBoundingClientRect().height;
+
+    // Section One
+    if (scrollY > headerHeight) {
+      const offset = scrollY - headerHeight;
+      const opacityValue = Math.max(0, 1 - (offset * 1.7) / sectionOneHeight);
+      const progress = offset / sectionOneHeight;
+
+      sectionOneHeaders.forEach((header, index) => {
+        const translateXValue = index === 0 ? offset * 2.5 : offset;
+        if (index == 1) {
+          header.style.transform = `rotate(350deg) translateY(-45%) translateX(${translateXValue}px)`;
+          header.style.opacity = opacityValue;
+        } else {
+          header.style.transform = `translateX(${translateXValue}px)`;
+          header.style.opacity = opacityValue;
+        }
+      });
+
+      sectionOneP.style.transform = `translateX(${offset * 2.2}px)`;
+      sectionOneP.style.opacity = opacityValue;
+
+      sectionOneLogo.style.opacity = opacityValue;
+      sectionOneLogo.style.transform = `scale(${1 - progress * 0.2})`;
+    } else {
+      resetSectionOneStyles(sectionOneHeaders, sectionOneLogo);
+    }
+
+    // Section Two
+    const sectionTwoStart = sectionOneHeight;
+    const sectionTwoEnd = sectionOneHeight * 2;
+    const sectionTwoProgress = (scrollY - sectionTwoStart) / sectionOneHeight;
+
+    console.log({scrollY, sectionTwoStart, sectionTwoProgress});
+
+    if (scrollY >= sectionTwoStart && scrollY <= sectionTwoEnd) {
+      // Translate from out of view to 0px
+      const translateYValue = Math.max(0, 100 - sectionTwoProgress * 100) - 100;
+      console.log({translateYValue});
+      const opacityValue = Math.max(0, 1 - sectionTwoProgress * 1.2);
+      const scaleValue = Math.max(0.2, 1 - sectionTwoProgress * 1.2);
+
+      sectionTwoHdr.style.transform = `translateY(${translateYValue}px)`;
+      sectionTwoHdr.style.opacity = opacityValue;
+
+      sectionTwoP.style.transform = `translateY(${translateYValue}px)`;
+
+      sectionTwoEl.style.transform = `scale(${scaleValue})`;
+      sectionTwoEl.style.opacity = opacityValue;
+    } else if (scrollY < sectionTwoStart - 20) {
+      // Reset H1 styles
+      sectionTwoHdr.style.transform = `translateY(-${sectionOneHeight}px)`;
+      sectionTwoHdr.style.opacity = 0.2;
+
+      // Reset P styles
+      sectionTwoP.style.transform = "translateY(600px)";
+      sectionTwoP.style.opacity = 1;
+
+      // Reset Section styles
+      sectionTwoEl.style.transform = `scale(1)`;
+      sectionTwoEl.style.opacity = 1;
+    } else if (scrollY > sectionTwoEnd) {
+      // Reset styles when out of range
+      resetSectionTwoStyles(sectionTwoHdr, sectionTwoP);
+      sectionTwoEl.style.transform = `scale(1)`;
+      sectionTwoEl.style.opacity = 1;
+    }
+  });
+});
+
+function resetSectionOneStyles(headers, logo, p) {
+  headers.forEach((header) => {
+    if (index == 1) {
+      header.style.transform = `rotate(350deg) translateY(-45%) translateX(0)`;
+      header.style.opacity = 1;
+    } else {
+      header.style.transform = "translateX(0)";
+      header.style.opacity = 1;
+    }
+  });
+  p.style.transform = "translateX(0)";
+  p.style.opacity = 1;
+  logo.style.opacity = 1;
+  logo.style.transform = "scale(1)";
+}
+
+function resetSectionTwoStyles(header, paragraph) {
+  header.style.transform = "translateY(0) scale(1)";
+  header.style.opacity = 1;
+
+  paragraph.style.transform = "translateY(0) scale(1)";
+  paragraph.style.opacity = 1;
+}
